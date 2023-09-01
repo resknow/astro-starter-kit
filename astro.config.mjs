e1edd7b53b43d5c12d292a5493ec1fa952a03db7
Chris Galbraith <chris@resknow.co.uk>
2023-09-01 13:52:42 +0100

    Upgrade to Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 import site from './src/site.js';
@@ -12,7 +11,5 @@ export default defineConfig({
   site: site.url,
   integrations: [tailwind({
     applyBaseStyles: false
-  }), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap(), partytown(), mdx()]
 });
